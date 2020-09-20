4226ae3b7e6d36ac8c95316a03452d8f62695996
Andy J Rivera <rivera.andy40@gmail.com>
2020-09-20 00:19:18 -0400

    Update ShoppingBag.java

--- ShoppingBag.java
+++ ShoppingBag.java
@@ -1,8 +1,5 @@
 package shoppingProject;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ShoppingBag {
 	private GroceryItem[] bag;
 	private int size;
@@ -13,35 +10,35 @@ public class ShoppingBag {
 		size = 0;
 		capacity = 5;
 	}
-	
-	private int find(GroceryItem item) {	//helper method to find an item
-		for(int i=0; i < size; i++) {
-			if(bag[i].equals(item)) {
+
+	private int find(GroceryItem item) { // helper method to find an item
+		for (int i = 0; i < size; i++) {
+			if (bag[i].equals(item)) {
 				return i;
 			}
 		}
 		return -1;
 	}
 
-	private void grow() {	//helper method to grow the capacity
+	private void grow() { // helper method to grow the capacity
 		GroceryItem[] largerBag = new GroceryItem[capacity + 5];
 		System.arraycopy(bag, 0, largerBag, 0, bag.length);
 		bag = largerBag;
 		capacity += 5;
 	}
-	
+
 	public void add(GroceryItem item) {
 		bag[size] = item;
 		size++;
-		
-		if(size == capacity) {	//this can go before or after adding item to list
+
+		if (size == capacity) { // this can go before or after adding item to list
 			grow();
 		}
 	}
-	
+
 	public boolean remove(GroceryItem item) {
 		int itemIndex = find(item);
-		if(itemIndex == -1) {
+		if (itemIndex == -1) {
 			return false;
 		}
 		bag[itemIndex] = bag[size - 1];
@@ -49,31 +46,45 @@ public class ShoppingBag {
 		size--;
 		return true;
 	}
-	
+
 	public double salesPrice() {
 		double totalPrice = 0;
-		for(int i=0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			totalPrice += bag[i].getPrice();
 		}
 		return totalPrice;
 	}
-	
+
 	public double salesTax() {
 		double totalTax = 0;
-		for(int i=0; i < size; i++) {
-			if(bag[i].isTaxable()) {
-				totalTax = bag[i].getItemTax();
+		for (int i = 0; i < size; i++) {
+			if (bag[i].isTaxable()) {
+				totalTax += bag[i].getItemTax();
 			}
 		}
 		return totalTax;
 	}
-	
+
 	public void print() {
 		System.out.println("**You have " + size + " item(s) in the bag:");
-		for(int i=0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			System.out.println(bag[i].toString());
 		}
 		System.out.println("**End of list");
 	}
+
+	public void checkout() {
+		System.out.println("**Checking out " + size + " items.");
+		for (int i = 0; i < size; i++) {
+			System.out.println(bag[i].toString());
+		}
+	}
 	
+	public int getSize() {
+		return size;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
 }
